72dbcddf1a4f1472b7c1a35d8a4aa9c7603f91f1
Thomas Neumann <thomas.neumann@axtesys.at>
2019-11-12 18:26:16 +0000

    Added simple movement

--- Assets/Scripts/Gonia/Character/Tool/ToolController.cs
+++ Assets/Scripts/Gonia/Character/Tool/ToolController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.Assertions.Must;
 using UnityEngine.EventSystems;
 
 namespace Gonia.Character.Tool
@@ -11,12 +12,11 @@ namespace Gonia.Character.Tool
         private float _origDrag;
         private float _origMass;
 
-        public OVRInput.Controller controller;
+        public OVRInput.Controller primaryController;
         public OVRInput.Controller secondaryController;
         public Camera centerEye;
         public Transform trackingSpace;
         public LineRenderer lineRendererTranslate;
-        public LineRenderer lineRendererRotate;
         public float maxGrabDistance = 10;
         public float stickSensitivity = 0.1f;
         public float minGrabDistance = 0.3f;
@@ -29,18 +29,33 @@ namespace Gonia.Character.Tool
 
         public float rotationStickSensitivity = 1;
         public float rotationHandSensitivity = 1;
-        public float clampRotation = 45;
 
         private bool _grabbing;
         private bool _triggeredGrab;
         private bool _triggeredRotate;
+        private bool _startRotate;
         private float _grabDistance;
 
         private float _rotateZLast;
-
+        
         private void FixedUpdate()
         {
-            if (OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, controller) >= triggerThreshold)
+            if (!_triggeredRotate)
+            {
+                Vector2 stickInput = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick, secondaryController);
+                Vector3 dir = new Vector3(stickInput.x, 0, stickInput.y);
+                dir = Quaternion.Euler(0, centerEye.transform.rotation.eulerAngles.y, 0) * dir;
+                var forward = centerEye.transform.forward;
+                trackingSpace.position += dir;
+            }
+
+            float turn = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick, primaryController).x;
+            if (Mathf.Abs(turn) >= 0.1)
+            {
+                trackingSpace.RotateAround(trackingSpace.position, Vector3.up, turn);
+            }
+            
+            if (OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, primaryController) >= triggerThreshold)
             {
                 if (_triggeredGrab) return;
                 if (_grabbing)
@@ -56,9 +71,9 @@ namespace Gonia.Character.Tool
                 }
                 else
                 {
-                    Vector3 pos = trackingSpace.TransformPoint(OVRInput.GetLocalControllerPosition(controller));
+                    Vector3 pos = trackingSpace.TransformPoint(OVRInput.GetLocalControllerPosition(primaryController));
                     Vector3 rotEuler =
-                        trackingSpace.TransformDirection(OVRInput.GetLocalControllerRotation(controller).eulerAngles);
+                        trackingSpace.TransformDirection(OVRInput.GetLocalControllerRotation(primaryController).eulerAngles);
                     Quaternion rot = Quaternion.Euler(rotEuler);
                     Ray ray = new Ray(pos, rot * Vector3.forward);
                     if (Physics.Raycast(ray, out var hit, maxGrabDistance))
@@ -85,13 +100,13 @@ namespace Gonia.Character.Tool
             {
                 _triggeredGrab = false;
             }
-
+            
             if (OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, secondaryController) >= triggerThreshold)
             {
                 if (_triggeredRotate) return;
                 if (!_grabbing) return;
 
-                _rotateZLast = OVRInput.GetLocalControllerRotation(secondaryController).z;
+                _startRotate = true;
 
                 _triggeredRotate = true;
             }
@@ -99,46 +114,56 @@ namespace Gonia.Character.Tool
             {
                 _triggeredRotate = false;
             }
+
+          
         }
 
         private void Update()
         {
             if (_grabbing)
             {
-                Vector2 distance = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick, controller);
+                Vector2 distance = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick, primaryController);
                 _grabDistance = Mathf.Min(Mathf.Max(minGrabDistance, _grabDistance + distance.y * stickSensitivity),
                     maxGrabDistance);
 
-                Vector3 pos = trackingSpace.TransformPoint(OVRInput.GetLocalControllerPosition(controller));
+                Vector3 pos = trackingSpace.TransformPoint(OVRInput.GetLocalControllerPosition(primaryController));
                 lineRendererTranslate.SetPosition(0, transform.position);
                 lineRendererTranslate.SetPosition(1, grabbedObject.transform.position);
 
                 Vector3 rotEuler =
-                    trackingSpace.TransformDirection(OVRInput.GetLocalControllerRotation(controller).eulerAngles);
+                    trackingSpace.TransformDirection(OVRInput.GetLocalControllerRotation(primaryController).eulerAngles);
                 Quaternion rot = Quaternion.Euler(rotEuler);
                 _grabbedRigidbody.AddForce(
                     (pos + rot * Vector3.forward * _grabDistance - grabbedObject.transform.position) * forceMult);
 
+                
+                
                 if (_triggeredRotate)
                 {
+                    if (_startRotate)
+                    {
+                        _rotateZLast = OVRInput.GetLocalControllerRotation(secondaryController).eulerAngles.z;
+                        _startRotate = false;
+                    }
+                    
                     Vector3 rotSecond = OVRInput.GetLocalControllerRotation(secondaryController).eulerAngles;
                     Vector2 stickInput = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick, secondaryController);
 
                     var grabPos = grabbedObject.transform.position;
-                    grabbedObject.transform.RotateAround(grabPos, centerEye.transform.forward, rotSecond.z - _rotateZLast);
+                    grabbedObject.transform.RotateAround(grabPos, centerEye.transform.forward, (rotSecond.z - _rotateZLast) * rotationHandSensitivity);
 
                     _rotateZLast = rotSecond.z;
                     
-                    grabbedObject.transform.RotateAround(grabPos, centerEye.transform.up, stickInput.x);
-                    grabbedObject.transform.RotateAround(grabPos, centerEye.transform.right, stickInput.y);
+                    grabbedObject.transform.RotateAround(grabPos, centerEye.transform.up, stickInput.x * rotationStickSensitivity);
+                    grabbedObject.transform.RotateAround(grabPos, centerEye.transform.right, stickInput.y * rotationStickSensitivity);
                 }
             }
-            else if (OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, controller) >= previewThreshold)
+            else if (OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, primaryController) >= previewThreshold)
             {
                 lineRendererTranslate.enabled = true;
-                Vector3 pos = trackingSpace.TransformPoint(OVRInput.GetLocalControllerPosition(controller));
+                Vector3 pos = trackingSpace.TransformPoint(OVRInput.GetLocalControllerPosition(primaryController));
                 Vector3 rotEuler =
-                    trackingSpace.TransformDirection(OVRInput.GetLocalControllerRotation(controller).eulerAngles);
+                    trackingSpace.TransformDirection(OVRInput.GetLocalControllerRotation(primaryController).eulerAngles);
                 Quaternion rot = Quaternion.Euler(rotEuler);
                 lineRendererTranslate.SetPosition(0, pos);
                 lineRendererTranslate.SetPosition(1, pos + rot * Vector3.forward * maxGrabDistance);
